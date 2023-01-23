bc4227dfca1b339c16c126eab3c6dfbb60b4355e
Alexander Voevodin <43845313+windcrack@users.noreply.github.com>
2023-01-23 17:17:09 +0300

    start pet project

---
+++ src/compents/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header(){
+    return(
+        <nav className='container'>
+            <div className="logo">
+                <a href="http://" className="logo__link">
+                    QPICK
+                </a>
+            </div>
+            
+        </nav>
+    )
+}
+
+export default Header;
---
+++ src/ui/select/Select.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Select(){
+    return(
+        <select>
+            <option value=""></option>
+        </select>
+    )
+}
+
+export default Select;
