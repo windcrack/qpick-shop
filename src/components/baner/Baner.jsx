de8422468c39cd4199de4293a4b6bb4c33eedce9
Alexander Voevodin <43845313+windcrack@users.noreply.github.com>
2023-01-30 10:01:55 +0300

    more update

--- src/components/baner/Baner.jsx
+++ src/components/baner/Baner.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import banerImg from '../../assets/img/baner/img1.png';
 
+import './style.sass';
+
 function Baner(){
     return(
         <div className='baner'>
             <div className="baner__body">
-                <h2 className="baner__text">Аксессуары для Iphone 13 Pro Max</h2>
+                <div className="baner__text"><h2>Аксессуары для <br /> Iphone 13 Pro Max</h2></div>
                 <img src={banerImg} alt="Картинка" className="baner__pick" />
             </div>
         </div>
